f858e5b9ddfb0d241b8cb1133c771001ca2b8226
Robert Hernandez <rhernandez513@gmail.com>
2015-09-08 00:33:32 -0500

    major progress, added fancybox

--- js/scripts.js
+++ js/scripts.js
@@ -141,3 +141,44 @@ $(window).load(function() {
 	});
 
 });
+ 
+/***************** FancyBox ******************/
+$(document).ready(function() {
+  $(".fancybox").fancybox();
+  $(".single_image_1").fancybox({
+    helpers: {
+      title : {
+        type : 'float'
+      }
+    }
+  });
+
+  $("#single_2").fancybox({
+    openEffect: 'elastic',
+    closeEffect: 'elastic',
+
+    helpers : {
+      title : {
+        type : 'inside'
+      }
+    }
+  });
+
+  $("#single_3").fancybox({
+    openEffect : 'none',
+    closeEffect: 'none',
+    helpers : {
+      title : {
+        type : 'outside'
+      }
+    }
+  });
+
+  $("#single_4").fancybox({
+    helpers : {
+      title : {
+        type : 'over'
+      }
+    }
+  });
+});
